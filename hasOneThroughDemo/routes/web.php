aa37178b92d1df36565b0bc26c26ca9ebf6ce65f
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-10-23 12:19:14 +0600

    update

--- hasOneThroughDemo/routes/web.php
+++ hasOneThroughDemo/routes/web.php
@@ -1,7 +1,12 @@
+// routes/web.php
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CountryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/countries', [CountryController::class, 'index']);
