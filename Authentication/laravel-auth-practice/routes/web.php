0524e163fe5560c362d9231fbf9ca64081093101
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-10-29 10:15:20 +0600

    controller created

--- Authentication/laravel-auth-practice/routes/web.php
+++ Authentication/laravel-auth-practice/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +19,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+Route::middleware(['auth','verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
